32c1f2e0e361707807cf8477740d0ee390e2cb73
dijkpicy <dijkspicy@outlook.com>
2018-01-05 17:56:58 +0800

    Create TopoSorting.java

---
+++ com/dijkspicy/gambol/model/TopoSorting.java
@@ -0,0 +1,73 @@
+package com.dijkspicy.gambol.model;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * com.dijkspicy.gambol.model
+ *
+ * @author t00321127
+ * @date 2018/1/4
+ */
+public class TopoSorting<T extends TopoNode> {
+    protected final List<T> nodes;
+    protected final Map<Object, T> depToNodeMap = new HashMap<>();
+    protected final Function<T, Object> depMapper;
+
+    public TopoSorting(List<T> nodes, Function<T, Object> depMapper) {
+        this.nodes = nodes;
+        this.depMapper = depMapper;
+        this.nodes.forEach(node -> depToNodeMap.put(this.getDep(node), node));
+    }
+
+    public TopoSorting(List<T> nodes) {
+        this(nodes, TopoNode::getId);
+    }
+
+    void sort() {
+        if (nodes == null || nodes.isEmpty()) {
+            return;
+        }
+
+        synchronized (this.nodes) {
+            List<T> nodesTemp = new LinkedList<>(nodes);
+
+            int index = 0;
+            List<T> vertices;
+            while (!(vertices = this.chooseVertices(nodesTemp)).isEmpty()) {
+                for (T vertex : vertices) {
+                    nodes.set(index++, vertex);
+                }
+            }
+
+            if (!nodesTemp.isEmpty()) {
+                throw new RuntimeException("nodes has cycles: " + nodesTemp);
+            }
+        }
+    }
+
+    protected List<T> chooseVertices(List<T> nodesTemp) {
+        List<T> vertices = new LinkedList<>();
+        nodesTemp.removeIf(vertex -> {
+            List<?> dependencies = vertex.getDependencies();
+            boolean isVertex = this.isTargetsEmptyOrRemoved(dependencies);
+            if (isVertex) {
+                vertices.add(vertex);
+            }
+            return isVertex;
+        });
+        vertices.forEach(vertex -> this.depToNodeMap.remove(this.getDep(vertex)));
+        return vertices;
+    }
+
+    private boolean isTargetsEmptyOrRemoved(List<?> dependencies) {
+        return dependencies == null || dependencies.isEmpty() || dependencies.stream().noneMatch(this.depToNodeMap::containsKey);
+    }
+
+    private Object getDep(T vertex) {
+        return depMapper.apply(vertex);
+    }
+}
