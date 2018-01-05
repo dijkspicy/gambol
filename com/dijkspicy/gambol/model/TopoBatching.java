36645ff5fb0dccc61289589a40a66b9d2b776351
dijkpicy <dijkspicy@outlook.com>
2018-01-05 17:56:20 +0800

    Create TopoBatching.java

---
+++ com/dijkspicy/gambol/model/TopoBatching.java
@@ -0,0 +1,62 @@
+package com.dijkspicy.gambol.model;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.function.Function;
+
+/**
+ * com.dijkspicy.gambol.model
+ *
+ * @author t00321127
+ * @date 2018/1/4
+ */
+public class TopoBatching<T extends TopoNode> extends TopoSorting<T> {
+
+    public TopoBatching(List<T> nodes, Function<T, Object> depMapper) {
+        super(nodes, depMapper);
+    }
+
+    public TopoBatching(List<T> nodes) {
+        this(nodes, TopoNode::getId);
+    }
+
+    public <B extends Batch> Queue<B> batch(Function<List<Queue<T>>, B> batchMapper) {
+
+        Queue<B> batches = new LinkedList<>();
+        synchronized (this.nodes) {
+            List<T> nodesTemp = new LinkedList<>(nodes);
+
+            List<T> vertices;
+            while (!(vertices = this.chooseVertices(nodesTemp)).isEmpty()) {
+                List<Queue<T>> batch = new ArrayList<>();
+                vertices.forEach(vertex -> {
+                    Queue<T> serialNodes = new LinkedList<>();
+                    serialNodes.add(vertex);
+
+                    T temp = vertex;
+                    while (temp.getFollowing().size() == 1) {
+                        Object dep = temp.getFollowing().get(0);
+                        T follow = this.depToNodeMap.get(dep);
+                        if (follow.getDependencies().size() == 1) {
+                            nodesTemp.remove(follow);
+                            this.depToNodeMap.remove(dep);
+                            serialNodes.add(follow);
+                            temp = follow;
+                        } else {
+                            break;
+                        }
+                    }
+                    batch.add(serialNodes);
+                });
+                batches.add(batchMapper.apply(batch));
+            }
+
+            if (!nodesTemp.isEmpty()) {
+                throw new RuntimeException("nodes has cycles: " + nodesTemp);
+            }
+        }
+        return batches;
+    }
+}
